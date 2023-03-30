2d2086e238932b4f22f77bff2127286f6af9c557
renjithmp <renjithmac@gmail.com>
2023-03-30 22:18:27 +0530

    migration for customer transactions

---
+++ CustomerCore/Migrations/20230330163751_customerTransactions.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace CustomerCore.Migrations
+{
+    /// <inheritdoc />
+    public partial class customerTransactions : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CustomerTransactions",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    customerId = table.Column<int>(type: "integer", nullable: false),
+                    transactionId = table.Column<int>(type: "integer", nullable: false),
+                    transactionType = table.Column<string>(type: "text", nullable: false),
+                    transactionTimestamp = table.Column<DateTime>(type: "timestamp without time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CustomerTransactions", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CustomerTransactions");
+        }
+    }
+}
